46bfdd682c0ce1b935a70ecc619471554f4d29e3
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2017-07-04 13:26:19 +0500

    added completely generic views

--- snippets/views.py
+++ snippets/views.py
@@ -5,37 +5,17 @@ from rest_framework import mixins
 from rest_framework import generics
 
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     '''
     List all snippets, or create a new snippet
     '''
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, format=None):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, format=None):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     '''
     Retrieve, update or delete a code snippet 
     '''
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, pk, format=None):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, pk, format=None):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, pk, format=None):
-        return self.destroy(request, *args, **kwargs)
