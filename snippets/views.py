b1ddd40c098768a7cd380d4e71c62cff28d90a3b
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2017-07-04 18:08:34 +0500

    minor fixes

--- snippets/views.py
+++ snippets/views.py
@@ -38,7 +38,7 @@ class SnippetViewSet(viewsets.ModelViewSet):
     @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
     def highlight(self, request, *args, **kwargs):
         snippet = self.get_object()
-        return Response(snippet.highighted)
+        return Response(snippet.highlighted)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
